c7fd6b2426d32097da7597cf53c29120e2e889d5
Michael Foos <michaelpfoos@gmail.com>
2021-09-14 15:25:44 -0400

    Commiting fizzbuzz challenge

---
+++ Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,21 @@
+function fizzbuzz() {
+    for (var i=1; i<=100; i++) {
+
+        if (i % 3 === 0 && i % 5 === 0) {
+            document.write("<p>FizzBuzz</p>");
+        }
+
+        else if (i % 3 === 0) {
+            document.write("<p>Fizz</p>");
+        }
+        else if (i % 5 === 0) {
+            document.write("<p>Buzz</p>");
+        }
+        
+        else {
+            document.write("<p>" + i + "</p>");
+        }
+    }
+}
+
+fizzbuzz();
