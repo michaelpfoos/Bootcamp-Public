cbd542cab58f87bcd642773280d389af7794cecd
Michael Foos <michaelpfoos@gmail.com>
2021-11-13 13:48:42 -0500

    changing ninja to super ninja

--- Ninja/nina.js
+++ Ninja/nina.js
@@ -22,7 +22,32 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name, 200);        
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    showStats() {
+        super.showStats();       
+    }
+
+    speakWisdom() {
+        super.drinkSake();
+        const message = "Why not learn Web Development at 40.  Youv'e got nothing better to do!";
+        console.log(message);
+    }
+
+}
+
 const ninja1 = new Ninja("Hyabusa", 100);
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+sensei1 = new Sensei("Bob");
+
+sensei1.speakWisdom();
+sensei1.showStats();
