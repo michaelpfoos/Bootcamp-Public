30f023162e8e6f3f3d25fa41957226c3601cb1fc
Michael Foos <michaelpfoos@gmail.com>
2021-11-13 07:15:05 -0500

    Updating users to include chaining

--- User/user.js
+++ User/user.js
@@ -7,12 +7,15 @@ class User {
     }
     makeDeposit(amount) {                // takes a parameter this is the amount of the deposit
         this.accountBalance += amount;   // the specific user's account increases by the amount of the value received
+        return this; //required for chaning.
     }
     makeWidthdrawl(amount) {
         this.accountBalance -= amount;
+        return this;
     }
     displayBalance() {
         console.log(`${this.name}'s Account Balance: ${this.accountBalance}`);
+        return this;
     }
     transferBalance(to, amount) {
         this.accountBalance -= amount;
@@ -20,6 +23,7 @@ class User {
         console.log(`${this.name} has transferred ${amount} to ${to.name}'s account.`);
         this.displayBalance();
         to.displayBalance();
+        return this;
     }
 
 }
@@ -30,25 +34,11 @@ carl = new User("Carl", "carl@somehwere.com");
 joe = new User("Joe", "joe@somewhere.edu");
 
 //first user will make 3 deposits and 1 withdrawl then dispaly their balance
-mike.makeDeposit(100);
-mike.makeDeposit(100);
-mike.makeDeposit(50);
-mike.displayBalance();
+mike.makeDeposit(100).makeDeposit(100).makeDeposit(50).displayBalance();
 
 //second user will make 2 deposits and 2 withdrawls and then display their balance
-carl.makeDeposit(100);
-carl.makeDeposit(100);
-carl.makeWidthdrawl(10);
-carl.makeWidthdrawl(20);
-carl.displayBalance();
-
+carl.makeDeposit(100).makeDeposit(100).makeWidthdrawl(10).makeWidthdrawl(20).displayBalance();
 
 //third user will make 1 deposit and 3 withdrawls then display their balance
-joe.makeDeposit(1000);
-joe.makeWidthdrawl(100);
-joe.makeWidthdrawl(100);
-joe.makeWidthdrawl(200);
-joe.displayBalance();
-
-joe.transferBalance(mike, 100);
+joe.makeDeposit(1000).makeWidthdrawl(100).makeWidthdrawl(100).makeWidthdrawl(200).displayBalance().transferBalance(mike, 100);
 
